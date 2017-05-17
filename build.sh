def685d2ac7d61afde73e24e5d570af90a6e5098
Alain Pannetrat <L1L1@gmx.com>
2017-05-17 17:47:08 +0300

    prepare for branching arduino package

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=0.0.6
+VERSION=0.0.7
 ZIPFILE=boards/nocan_pack_$VERSION.zip
 JSONFILE=package_omzlo.com_index.json
 TEMPLATE=package_omzlo.com_index.template.json
@@ -21,7 +21,5 @@ SIZE=`stat -f "%z" $ZIPFILE`
 sed -e "s/\${version}/$VERSION/g" -e "s/\${size}/$SIZE/g" -e "s/\${checksum}/$CHECKSUM/g" $TEMPLATE >  $JSONFILE
 
 rm -rf $VERSION/
-
-scp $ZIPFILE $JSONFILE pannetra@paris:/home/pannetra/public_html/
 
 echo "Updated version $VERSION with checksum $CHECKSUM"
